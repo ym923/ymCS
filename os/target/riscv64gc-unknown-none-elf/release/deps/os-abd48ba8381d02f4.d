283f05babe85417f055c0a30fd425b9945faac4c
ym923 <voila666@163.com>
2025-04-28 11:04:54 +0800

    22301108-杨蔓-2025-04-28

---
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
@@ -0,0 +1,5 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs
+
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/riscv64gc-unknown-none-elf/release/os.d
@@ -0,0 +1,1 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/release/os: /mnt/os/src/main.rs
