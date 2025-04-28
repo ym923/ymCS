0af8895e01917c7cade13aa76affad3b7997fe33
ym923 <voila666@163.com>
2025-04-28 17:12:30 +0800

    22301108-杨蔓-实验2-2025-04-28

--- os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
+++ os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d
@@ -1,5 +1,9 @@
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
-/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs
+/mnt/os/target/riscv64gc-unknown-none-elf/release/deps/os-abd48ba8381d02f4.d: src/main.rs src/console.rs src/lang_items.rs src/sbi.rs src/entry.asm
 
 src/main.rs:
+src/console.rs:
+src/lang_items.rs:
+src/sbi.rs:
+src/entry.asm:
